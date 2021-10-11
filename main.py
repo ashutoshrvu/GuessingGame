17902cd07aba3de764df18ca217f59409fc8c61b
ashutoshrvu <ashutoshrvu@gmail.com>
2021-10-11 18:03:15 +0530

    Intial

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+# Guessing Game Of Wizard
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Defining variables
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input("I am the wizard. Guess the number, puny mortal: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("You won. I am impressed!")
+        break
+else:
+    print("Haha! Mortals can not learn the ways of magic!")
